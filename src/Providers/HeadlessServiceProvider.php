8bc456d2d4c86104adc794e7e38e0766c153aa4d
Mark Fluehmann <dbizapps@gmail.com>
2021-09-22 14:28:06 +0200

    Initial package development

---
+++ src/Providers/HeadlessServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace dbizapps\Headless\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use dbizapps\Headless\Console\ChromeDriverCommand;
+use dbizapps\Headless\Console\InstallDriverCommand;
+
+class HeadlessServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+    
+}
